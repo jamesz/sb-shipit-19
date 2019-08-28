cc80611d4c7a30e381861bc89611d4401ffaeb44
James Z <james.zhuo@gmail.com>
2019-08-29 09:07:56 +1000

    first commit

---
+++ src/extract_with_google.js
@@ -0,0 +1,20 @@
+const {log} = require('./logger');
+
+function extract_text() {
+
+}
+
+
+function extract() {
+    log('Initialising AI');
+    log('Starting extraction');
+    log('Loading sample documents');
+    log('Document X - extracting document text with azure text extraction service');
+    console.log('Document X - text extracted as per below');
+    console.log('xxxxxxxxxxxxxxxxxx');
+    console.log('xxxxxxxxxxxxxxxxxx');
+    console.log('xxxxxxxxxxxxxxxxxx');
+    log('Document X - extracting clauses with google machine line api')
+}
+
+extract();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('*****************************************')
+console.log('      Hello Smokeball ShipIt 2019!       ')
+console.log('*****************************************')
+console.log('');
+console.log('The following scripts are available!')
+console.log('* npm run extract_with_google');
+console.log('* npm run extract_with_regex');
