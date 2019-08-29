38e4c033b2901477b45daef69d525f5784a61cf4
James Z <james.zhuo@gmail.com>
2019-08-29 19:48:23 +1000

    added postgres config, not tested yet

--- src/services/clauseDb.js
+++ src/services/clauseDb.js
@@ -1,3 +1,14 @@
+var pg = require('knex')({
+    client: 'pg',
+    connection: {
+        host : 'smb-shipit0-19.czd71xsvhgrg.ap-southeast-2.rds.amazonaws.com',
+        user : 'postgres',
+        password : 'T3pL4rTOF2DfWjcz',
+        database : 'postgres'
+    },
+    searchPath: ['knex', 'public'],
+});
+
 function insert(newClauses) {
     console.log('inserting clauses to database');
 }
