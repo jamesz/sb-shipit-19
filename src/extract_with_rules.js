9792ada6cfb04232fbadfc6bcb9c33a3f20dcaf8
James Z <james.zhuo@gmail.com>
2019-08-29 16:12:02 +1000

    this would work better

--- src/extract_with_rules.js
+++ src/extract_with_rules.js
@@ -78,8 +78,8 @@ function extractClause(startLineIndex, endLineIndex, lines, clauses) {
 function extract() {
     logHeader('Starting extraction with rule-based engine');
 
-    // const fileNames = files.getSampleFileNames();
-    const fileNames = ['sample-one-liners.docx'];
+    const fileNames = files.getSampleFileNames();
+    // const fileNames = ['sample-one-liners.docx'];
     fileNames.forEach(fileName => {
         logHeader(fileName);
         const documentText = files.readDocumentText(fileName);
