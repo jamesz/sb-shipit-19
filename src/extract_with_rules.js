7981e0c216704ddeafa8c9162d30ab1a6776d2ab
James Z <james.zhuo@gmail.com>
2019-08-29 16:09:01 +1000

    Made clause extraction engine handle more scenarios e.g. ignore oneliners

--- src/extract_with_rules.js
+++ src/extract_with_rules.js
@@ -8,17 +8,24 @@ function cleanLine(lineText)
     return lineText;    
 }
 
+
 function extractClauses(fileName, documentText) {
     log(`${fileName} - extracting clauses with rules`)
 
+    // Clause Identification Rules
+    // 1) a clause always starts with a number at the beginning of the line
+    // 2) everything between the start of one clause line and another clause
+    // line will be considered a clause we want to extract
 
-    // 1) a clause always starts with a number
-    // 2) the end of the clause 
-    //    2.1 is the beginning of another clause
-    //    2.2 is a heading/bold text/ starting on a new line
     // Handling Anomalies 
     // a) how to ignore table of contents
-    // 
+    //    Solution: we'll ignore all one liner clauses except the last line 
+    //              as these are trivial to recreate for the customer
+    // b) how to handle text that don't belong to an actual clause
+    //    Solution: we'll ignore these and let the customer decide whether
+    //              to cut them out from an inserted clause
+
+    // split text file into lines
     const lines = documentText.split('\n');
     const clauseLineIndexes = [];
     const clauses = []
@@ -31,38 +38,48 @@ function extractClauses(fileName, documentText) {
             clauseLineIndexes.push(index);
        };
     });
-    // clauseLineIndexes.forEach(lineIndex => console.log(lines[lineIndex]));
 
-    // extract all clauses
+    // extract each identified clauses
     clauseLineIndexes.forEach((clauseLineIndex, i) => {
         // 1) ignore all one line clauses as they are too trivial 
         // and the customer can write it themselves, these are also easy 
         // to create if we ever add the ability to add custom clauses
         // 2) extract everything between two potential clause
-        if (clauseLineIndex === lines.length) {
-            clauses.push(lines[clauseLineIndex]); // last line
-        } else if (clauseLineIndex < lines.length - 1 && i < clauseLineIndexes.length - 1) {
-            const linesInClause = []
-            const nextClauseLineIndex = clauseLineIndexes[i+1];
-            for(let current = clauseLineIndex; current < nextClauseLineIndex; current++) {
-                linesInClause.push(lines[current]);
+        if (clauseLineIndex === lines.length - 1) { // clause is last line
+            clauses.push(lines[clauseLineIndex]); 
+        } else if (clauseLineIndex < lines.length - 2) { // clause line is before or equal second last line
+            if (i === clauseLineIndexes.length - 1) { 
+                // is last clause - extract to end of file
+                extractClause(clauseLineIndex, lines.length - 1, lines, clauses);
+            } else if (i < clauseLineIndexes.length - 1) { 
+                // is not last clause
+                const nextClauseLineIndex = clauseLineIndexes[i+1];
+                extractClause(clauseLineIndex, nextClauseLineIndex - 1, lines, clauses);
             }
-            const clause = linesInClause.join('\n');
-            clauses.push(clause);
-        }
+        } 
     });
 
-    clauses.forEach(clause => console.log(clause, '\n----------\n'));
+    clauses.forEach(clause => console.log(clause, '\n-------------------------------------------------------------------------------------------------------------------------\n'));
+    return clauses;
+}
+
+function extractClause(startLineIndex, endLineIndex, lines, clauses) {
+    if (startLineIndex < endLineIndex) {
+        // i.e. not a one liner clause
+        const linesInClause = [];
+        for (let current = startLineIndex; current <= endLineIndex; current++) {
+            linesInClause.push(lines[current]);
+        }
+        const clause = linesInClause.join('\n');
+        clauses.push(clause);
+    }
 }
 
 function extract() {
     logHeader('Starting extraction with rule-based engine');
 
-    // clean up existing output files
-    //files.cleanOutputFiles();
-
-    // perform extract on all supported sample files
-    const fileNames = files.getSampleFileNames();
+    // const fileNames = files.getSampleFileNames();
+    const fileNames = ['sample-one-liners.docx'];
     fileNames.forEach(fileName => {
         logHeader(fileName);
         const documentText = files.readDocumentText(fileName);
