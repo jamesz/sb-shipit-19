42c4966244df0dba4ad294a56067f3f929d8910c
James Z <james.zhuo@gmail.com>
2019-08-30 10:54:00 +1000

    clean up

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const clauseDb = require('./services/clauseDb');
-
-const testRecord = clauseDb.createRecord(`
-some very long test clause text
-spread over multiple
-lines.
-`);
-
-clauseDb.insert([testRecord]);
